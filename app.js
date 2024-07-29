dce3848167d559e8784c995bea800f891d8094d1
Pooja19S <poojasomatkar19@gmail.com>
2024-07-29 16:27:42 +0530

    Updated two files.

--- app.js
+++ app.js
@@ -14,9 +14,9 @@ const port = 2000;
 // create connection to database
 // the mysql.createConnection function takes in a configuration object which contains host, user, password and the database name.
 const db = mysql.createConnection ({
-    host: 'localhost',
-    user: 'root',
-    password: '',
+    host: 'database-1.cd226aqeqvp0.ap-south-1.rds.amazonaws.com',
+    user: 'admin',
+    password: 'aE2F7dTESwUSU8cWPp0I',
     database: 'socka'
 });
 
